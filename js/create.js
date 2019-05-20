0e8f5d28189e8ab2b6bd844ad38bd0da4ff6a891
MatsudaAtsushi <akiyam1028@gmail.com>
2019-05-20 10:03:41 +0900

    各種ファイルを作成

---
+++ js/create.js
@@ -0,0 +1,33 @@
+// 新着情報を追加
+async function postEvent() {
+  // 入力内容を取得
+  var form = document.forms.create_event;
+  var body = {
+    title: form.title.value,
+    started_at: form.started_at.value,
+    ended_at: form.ended_at.value,
+    description: form.description.value
+  };
+  // ヘッダーを設定
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+  headers.append('Authorization', 'Bearer cf4bd65638b811e8840a1f128ca65a99');
+  // APIを呼び出す
+  var res = await fetch('http://192.168.33.10/api/events', {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  });
+  // 登録できたかどうかをお知らせ
+  var result = document.querySelector('#result');
+  if (res.ok) {
+    result.innerHTML = '登録しました。';
+  } else {
+    result.innerHTML = '失敗しました。';
+  }
+}
+
+document.querySelector('#create_event button').onclick = function() {
+  postEvent();
+}
---
+++ js/main.js
@@ -0,0 +1,45 @@
+// 新着情報の全体データを取得
+async function getEventAll() {
+  // APIを呼び出す
+  var res = await fetch('http://192.168.33.10/api/events');
+  // 呼び出したAPIからデータを取得
+  var data = await res.json();
+  // 取得したデータを開始日時の新しい順に並び替え
+  data.sort(function(a, b) {
+    return (a.started_at < b.started_at ? 1 : -1);
+  });
+  // タイトルのみを5レコード表示
+  for (i = 0; i < 5; i++) {
+    var title = '<a href="#" id="' + data[i].id + '">' + data[i].title + '</a>';
+    var html = '<li>' + title + '</li>';
+    // <ul id="#events">に追加
+    document.querySelector('#events').insertAdjacentHTML('beforeend', html);
+  }
+  // リンクをクリックしたときに詳細情報を表示
+  var links = document.querySelectorAll('#events a');
+  for (i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+      getEvent(this.id);
+    }
+  }
+}
+
+// 新着情報の個別データを取得
+async function getEvent(id) {
+  var res = await fetch('http://192.168.33.10/api/events/' + id);
+  var data = await res.json();
+  // 取得した情報を指定したエリアに表示
+  document.querySelector('#event_detail h3').innerHTML = data.title;
+  document.querySelector('#event_detail .description').innerHTML = data.description;
+  document.querySelector('#event_detail .started_at').innerHTML = '開始日時：' + getDateTime(data.started_at);
+  document.querySelector('#event_detail .ended_at').innerHTML = '終了日時：' + getDateTime(data.ended_at);
+}
+
+// 日時を分かりやすい形式に変換
+function getDateTime(datetime) {
+  var date = new Date(datetime);
+  return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+}
+
+// 関数を呼び出す必要がある
+getEventAll();
