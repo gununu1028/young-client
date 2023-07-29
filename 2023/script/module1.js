3ab55635334759daf992b56222d00327ae01af5e
AtsushiMatsuda <akiyam1028@gmail.com>
2023-07-29 09:57:24 +0900

    キャラクターを動かせるようにする

--- 2023/script/module1.js
+++ 2023/script/module1.js
@@ -14,7 +14,49 @@ app = Vue.createApp({
         character_position_style() {
             return { 'left': this.character_position.x + 'px', 'top': this.character_position.y + 'px' }
         },
+        // ここから追記
+        push_key(event) {
+            this.update_character_position(event.code);
+        },
+        update_character_position(code) {
+            move_amount = 10;
+            switch (code) {
+                case 'ArrowUp':
+                    this.character_position.y -= move_amount;
+                    break;
+                case 'ArrowRight':
+                    this.character_position.x += move_amount;
+                    break;
+                case 'ArrowDown':
+                    this.character_position.y += move_amount;
+                    break;
+                case 'ArrowLeft':
+                    this.character_position.x -= move_amount;
+                    break;
+            }
+            this.character_position.x = Math.max(0, Math.min(this.character_position.x, 1024 - 200));
+            this.character_position.y = Math.max(0, Math.min(this.character_position.y, 768 - 50));
+            localStorage.setItem('character_position_x', this.character_position.x);
+            localStorage.setItem('character_position_y', this.character_position.y);
+        },
+        // ここまで追記（波括弧やカンマに注意）
     },
+    // ここから追記
+    async mounted() {
+        window.addEventListener('keydown', this.push_key);
+        url = 'https://japanskills2023.m5a.jp/api/dialogs';
+        response = await fetch(url);
+        this.dialogs_json = await response.json();
+        x = localStorage.getItem('character_position_x');
+        if (x) {
+            this.character_position.x = parseInt(x);
+        }
+        y = localStorage.getItem('character_position_y');
+        if (y) {
+            this.character_position.y = parseInt(y);
+        }
+    }
+    // ここまで追記
 });
 
 app.mount('#app');
