73c832376c547f0d38253419a89f70348360f689
松田淳司 <amatsuda@zaico.co.jp>
2025-07-10 19:57:13 +0900

    リファクタリング

--- 2025/script/game-component.js
+++ 2025/script/game-component.js
@@ -82,40 +82,54 @@ const GameScreen = {
             };
             
             const action = keyActions[event.code];
-            if (action) {
-                event.preventDefault();
-                action();
-            }
+            if (!action) return;
+            
+            event.preventDefault();
+            action();
         },
         
         // ゲーム開始/停止切り替え
         toggleGame() {
             if (!this.gameStarted) {
                 this.startGame();
-            } else {
-                this.pauseGame();
+                return;
             }
+            
+            this.pauseGame();
         },
         
         // ゲーム開始
         startGame() {
             this.gameStarted = true;
             this.isPaused = false;
-            this.gameLoop = setInterval(() => {
-                this.updateGame();
-            }, this.fieldSpeed);
+            this.startGameLoop();
         },
         
         // ゲーム一時停止
         pauseGame() {
             this.isPaused = !this.isPaused;
+            
             if (this.isPaused) {
-                clearInterval(this.gameLoop);
-            } else {
-                this.gameLoop = setInterval(() => {
-                    this.updateGame();
-                }, this.fieldSpeed);
+                this.stopGameLoop();
+                return;
             }
+            
+            this.startGameLoop();
+        },
+        
+        // ゲームループ開始
+        startGameLoop() {
+            this.gameLoop = setInterval(() => {
+                this.updateGame();
+            }, this.fieldSpeed);
+        },
+        
+        // ゲームループ停止
+        stopGameLoop() {
+            if (!this.gameLoop) return;
+            
+            clearInterval(this.gameLoop);
+            this.gameLoop = null;
         },
         
         // プレイヤー移動
@@ -123,7 +137,9 @@ const GameScreen = {
             if (!this.gameStarted || this.isPaused) return;
             
             const newPosition = this.playerPosition + direction;
-            if (newPosition >= 0 && newPosition < GAME_CONSTANTS.FIELD_WIDTH) {
+            const isValidPosition = newPosition >= 0 && newPosition < GAME_CONSTANTS.FIELD_WIDTH;
+            
+            if (isValidPosition) {
                 this.playerPosition = newPosition;
             }
         },
@@ -141,18 +157,35 @@ const GameScreen = {
             this.fieldOffset += GAME_CONSTANTS.MOVE_STEP;
             this.moveDistance += GAME_CONSTANTS.MOVE_STEP;
             
-            if (this.moveDistance >= GAME_CONSTANTS.BLOCK_SIZE) {
-                this.moveDistance = 0;
-                this.fieldOffset = 0;
-                this.blocksMovedCount++;
-                
-                this.displayField.shift();
-                const nextRowIndex = 12 + this.blocksMovedCount;
-                const nextRow = nextRowIndex < this.fieldData.length 
-                    ? [...this.fieldData[nextRowIndex]] 
-                    : [0, 0, 0];
-                this.displayField.push(nextRow);
-            }
+            if (this.moveDistance < GAME_CONSTANTS.BLOCK_SIZE) return;
+            
+            this.processBlockMovement();
+        },
+        
+        // ブロック移動処理
+        processBlockMovement() {
+            this.moveDistance = 0;
+            this.fieldOffset = 0;
+            this.blocksMovedCount++;
+            
+            this.addNewRow();
+        },
+        
+        // 新しい行を追加
+        addNewRow() {
+            this.displayField.shift();
+            
+            const nextRowIndex = 12 + this.blocksMovedCount;
+            const nextRow = this.createNextRow(nextRowIndex);
+            
+            this.displayField.push(nextRow);
+        },
+        
+        // 次の行を作成
+        createNextRow(rowIndex) {
+            return rowIndex < this.fieldData.length 
+                ? [...this.fieldData[rowIndex]] 
+                : [0, 0, 0];
         },
         
         // 衝突判定
@@ -169,21 +202,36 @@ const GameScreen = {
         
         // 衝突処理
         handleCollision(cellType) {
-            switch (cellType) {
-                case GAME_CONSTANTS.CELL_TYPES.OBSTACLE:
-                    if (this.isInvincible) {
-                        this.isInvincible = false;
-                    } else {
-                        this.endGame();
-                    }
-                    break;
-                case GAME_CONSTANTS.CELL_TYPES.SCORE_ITEM:
-                    this.score += GAME_CONSTANTS.ITEM_SCORE;
-                    break;
-                case GAME_CONSTANTS.CELL_TYPES.INVINCIBLE_ITEM:
-                    this.isInvincible = true;
-                    break;
+            const collisionHandlers = {
+                [GAME_CONSTANTS.CELL_TYPES.OBSTACLE]: () => this.handleObstacle(),
+                [GAME_CONSTANTS.CELL_TYPES.SCORE_ITEM]: () => this.handleScoreItem(),
+                [GAME_CONSTANTS.CELL_TYPES.INVINCIBLE_ITEM]: () => this.handleInvincibleItem()
+            };
+            
+            const handler = collisionHandlers[cellType];
+            if (handler) {
+                handler();
             }
+        },
+        
+        // 障害物との衝突処理
+        handleObstacle() {
+            if (this.isInvincible) {
+                this.isInvincible = false;
+                return;
+            }
+            
+            this.endGame();
+        },
+        
+        // スコアアイテム処理
+        handleScoreItem() {
+            this.score += GAME_CONSTANTS.ITEM_SCORE;
+        },
+        
+        // 無敵アイテム処理
+        handleInvincibleItem() {
+            this.isInvincible = true;
         },
         
         // スコア更新
@@ -195,13 +243,28 @@ const GameScreen = {
         
         // スピード上昇チェック
         checkSpeedIncrease() {
-            if (this.blocksMovedCount > 0 && this.blocksMovedCount % GAME_CONSTANTS.SPEED_INCREASE_BLOCKS === 0) {
-                this.fieldSpeed = Math.max(GAME_CONSTANTS.MIN_SPEED, this.fieldSpeed - 5);
-                clearInterval(this.gameLoop);
-                this.gameLoop = setInterval(() => {
-                    this.updateGame();
-                }, this.fieldSpeed);
-            }
+            const shouldIncrease = this.shouldIncreaseSpeed();
+            if (!shouldIncrease) return;
+            
+            this.increaseSpeed();
+        },
+        
+        // スピード上昇判定
+        shouldIncreaseSpeed() {
+            return this.blocksMovedCount > 0 && 
+                   this.blocksMovedCount % GAME_CONSTANTS.SPEED_INCREASE_BLOCKS === 0;
+        },
+        
+        // スピード上昇処理
+        increaseSpeed() {
+            this.fieldSpeed = Math.max(GAME_CONSTANTS.MIN_SPEED, this.fieldSpeed - 5);
+            this.restartGameLoop();
+        },
+        
+        // ゲームループ再開
+        restartGameLoop() {
+            this.stopGameLoop();
+            this.startGameLoop();
         },
         
         // ゲーム終了
@@ -218,10 +281,7 @@ const GameScreen = {
         
         // ゲーム終了処理
         cleanupGame() {
-            if (this.gameLoop) {
-                clearInterval(this.gameLoop);
-                this.gameLoop = null;
-            }
+            this.stopGameLoop();
         },
         
         // セルのCSSクラス取得
