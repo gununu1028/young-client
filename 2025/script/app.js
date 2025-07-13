504134b7eac81c87b418f7521d95285459d7f7dd
松田淳司 <amatsuda@zaico.co.jp>
2025-07-13 10:52:03 +0900

    ファイルを統合

--- 2025/script/app.js
+++ 2025/script/app.js
@@ -1,3 +1,32 @@
+// ゲームの設定値
+const GAME_CONSTANTS = {
+    FIELD_WIDTH: 3,
+    FIELD_HEIGHT: 12,
+    BLOCK_SIZE: 64,
+    PLAYER_ROW: 1,
+    INITIAL_SPEED: 100,
+    MIN_SPEED: 50,
+    SPEED_INCREASE_BLOCKS: 5,
+    MOVE_STEP: 8,
+    BLOCK_SCORE: 10,
+    ITEM_SCORE: 100,
+    CELL_TYPES: {
+        RIVER: 0,
+        OBSTACLE: 1,
+        SCORE_ITEM: 2,
+        INVINCIBLE_ITEM: 3
+    }
+};
+
+// サンプルフィールドデータ
+const SAMPLE_FIELD_DATA = [
+    [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
+    [0, 0, 0], [0, 1, 0], [0, 0, 0], [1, 0, 1], [0, 2, 0],
+    [0, 0, 0], [1, 0, 0], [0, 3, 0], [0, 0, 1], [2, 0, 0],
+    [0, 0, 0], [1, 1, 0], [0, 0, 0], [0, 0, 2], [0, 1, 0],
+    [3, 0, 0], [0, 0, 0], [0, 0, 0], [1, 0, 1], [0, 2, 0]
+];
+
 // Vue.jsアプリケーション初期化
 const { createApp } = Vue;
 const { createRouter, createWebHashHistory } = VueRouter;
